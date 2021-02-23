601311a6c4d5902e789056974283a3888531d4f2
Santiago Argilla <sargilla@gmail.com>
2021-02-23 11:39:35 -0300

    con mayusculas

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.listen(process.env.PORT || 3000,function()  {
 });
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+    res.sendFile(path.resolve(__dirname,'./Views/home.html'))
 })
 
 app.get('/register', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+    res.sendFile(path.resolve(__dirname,'./Views/register.html'))
 })
 
 app.get('/login', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+    res.sendFile(path.resolve(__dirname,'./Views/login.html'))
 })
