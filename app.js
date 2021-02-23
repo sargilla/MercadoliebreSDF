05a05e3fb3d7eac20e8747ade87f4fcfad3e1f05
Santiago Argilla <sargilla@gmail.com>
2021-02-23 11:43:26 -0300

    correccion css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, './Public');
 app.use (express.static(publicPath) );
 
 app.listen(process.env.PORT || 3000,function()  {
